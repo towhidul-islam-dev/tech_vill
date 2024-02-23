739b156f2f3679822838a9fdec47b536c4392359
towhidul-islam-dev <niamaaktar@gmail.com>
2024-02-23 23:51:57 +0600

    add custom asset to tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,44 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#2b57c5",
+        accent: "#0B7D5B",
+        baseClr1: "#f0f2f7",
+        denger: "#f05959",
+        natural1: "#ba9e9f",
+        natural2: "#332c23",
+        natural3: "#ffff",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "contact-image": "url('https://unsplash.it/1000/1000?image=789')",
+      },
+      minHeight: {
+        "custom-min-h": "500px",
+        "custom-h-form": "600px",
+      },
+      minWidth: {
+        "custome-w": "400px",
+      },
+      gridTemplateColumns: {
+        footerLayout: "repeat(auto-fit, minmax(14rem, 1fr))",
+        productLayout: "repeat(auto-fit,minmax(13rem, 1fr))",
+        productLayoutTop: "repeat(auto-fit,minmax(13rem, 1fr))",
+        relatedProductLayout: "repeat(auto-fit,minmax(200px, 1fr))",
+        collectionLayout: "repeat(auto-fit, minmax(12rem, 1fr))",
+        homepageLayoutHero: "repeat(auto-fit, minmax(22rem, 1fr))",
+        homepageLayoutHero1: "repeat(auto-fit, minmax(18rem, 1fr))",
+        userLayout: "repeat(auto-fit, minmax(5rem, 1fr))",
+      },
+      fontSize: {
+        extraSmall: "8px",
+        small: "10px",
       },
     },
   },
   plugins: [],
 };
+3;
